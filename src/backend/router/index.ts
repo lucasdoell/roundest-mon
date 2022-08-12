166b757ce86b168621950a34932c94a1b205b6e1
Lucas Doell <lmd248@cornell.edu>
2022-08-12 02:42:57 -0400

    send back less data

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -11,7 +11,7 @@ export const appRouter = trpc
       const api = new PokemonClient();
 
       const pokemon = await api.getPokemonById(input.id);
-      return pokemon;
+      return {name: pokemon.name, sprites: pokemon.sprites};
     }
   });
 
