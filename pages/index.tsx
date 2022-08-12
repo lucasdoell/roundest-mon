af2e55a5b6dcf939effdb6455142b935e08dbed3
Lucas Doell <lmd248@cornell.edu>
2022-08-12 01:11:38 -0400

    basic ui structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,16 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
-    <div className='text-2xl'>
-      Hello world
+    <div className='h-screen w-screen flex flex-col justify-center  items-center'>
+      <div className='text-2xl text-center'>Which Pokémon is rounder?</div>
+      <div className='p-2' />
+      <div className='border rounded p-8 flex justify-between max-w-2xl items-center'>
+        <div className='w-16 h-16 bg-red-200' />
+        <div className='p-8'>vs</div>
+        <div className='w-16 h-16 bg-red-200' />
+      </div>
     </div>
   )
 }
-
-export default Home
