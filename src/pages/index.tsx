a031db7c4fafd01f6c312360a092727bccdb1f4f
Lucas Doell <lmd248@cornell.edu>
2022-08-12 18:28:35 -0400

    added loading state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,29 +32,32 @@ export default function Home() {
     updateIds(getOptionsForVote());
   };
 
+  const dataLoaded =
+    !firstPokemon.isLoading &&
+    firstPokemon.data &&
+    !secondPokemon.isLoading &&
+    secondPokemon.data;
+
   return (
-    <div className="h-screen w-screen flex flex-col justify-center  items-center">
-      <div className="text-2xl text-center">Which Pokémon is rounder?</div>
-      <div className="p-2" />
-      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        {!firstPokemon.isLoading &&
-          firstPokemon.data &&
-          !secondPokemon.isLoading &&
-          secondPokemon.data && (
-            <>
-              <PokemonListing
-                pokemon={firstPokemon.data}
-                vote={() => voteForRoundest(first)}
-              />
-              <div className="p-8">vs</div>
-              <PokemonListing
-                pokemon={secondPokemon.data}
-                vote={() => voteForRoundest(second)}
-              />
-            </>
-          )}
-      </div>
-      <div className="absolute bottom-0 w-full text-xl text-center pb-2">
+    <div className="h-screen w-screen flex flex-col justify-between items-center">
+      <div className="text-2xl text-center pt-8">Which Pokémon is rounder?</div>
+      {dataLoaded && (
+        <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+          <>
+            <PokemonListing
+              pokemon={firstPokemon.data}
+              vote={() => voteForRoundest(first)}
+            />
+            <div className="p-8">vs</div>
+            <PokemonListing
+              pokemon={secondPokemon.data}
+              vote={() => voteForRoundest(second)}
+            />
+          </>
+        </div>
+      )}
+      {!dataLoaded && <img src="/rings.svg" className="w-48" />}
+      <div className="w-full text-xl text-center pb-2">
         <a href="https://github.com/lucasdoell/roundest-mon">GitHub</a>
         {" | "}
         <Link href="/results">Results</Link>
